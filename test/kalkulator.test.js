282937df53ee3030a25ae97651c37eafc81ea88c
trioadhi <trioadhipamungkas04@gmail.com>
2026-05-21 14:30:31 +0700

    gas

---
+++ test/kalkulator.test.js
@@ -0,0 +1,13 @@
+const {
+  tambah,
+  kurang,
+  //TODO Here...
+} = require("../src/kalkulator");
+
+describe("Kalkulator", () => {
+  test("Tambah", () => {
+    expect(tambah(10, 5)).toBe(15);
+  });
+  //TODO Here...
+  
+});
